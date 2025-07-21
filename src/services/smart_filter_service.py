a0a7a25823e933872ca41c77c66489399674f1c1
Ginters Blauva <ginters.blauva@sapiens.com>
2025-07-22 00:39:04 +0300

    add price per m2 filter

--- src/services/smart_filter_service.py
+++ src/services/smart_filter_service.py
@@ -1,4 +1,4 @@
-from config.config_reader import UNWANTED_DISTRICTS, LISTING_MIN_PRICE, LISTING_MAX_PRICE, LISTING_MIN_SIZE, LISTING_MAX_SIZE, LISTING_MIN_ROOMS, LISTING_MAX_ROOMS
+from config.config_reader import UNWANTED_DISTRICTS, LISTING_MIN_PRICE, LISTING_MAX_PRICE, LISTING_MIN_SIZE, LISTING_MAX_SIZE, LISTING_MIN_ROOMS, LISTING_MAX_ROOMS, LISTING_MIN_PRICE_PER_M2
 
 # TODO - Add AI, add room filter
 def filterListings(listings):
@@ -19,6 +19,10 @@ def filterListings(listings):
 
         # Room filter
         if not (LISTING_MIN_ROOMS <= listing['rooms'] <= LISTING_MAX_ROOMS):
+            continue
+
+        # Price per m2 filter
+        if (float(listing['price']) / float(listing['size']) < LISTING_MIN_PRICE_PER_M2):
             continue
 
         # Passed all filters
